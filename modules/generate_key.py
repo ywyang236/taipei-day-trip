4664a96d96623ae02d588e7ba046f576f9169c28
ywyang236 <ywyang236@gmail.com>
2023-10-08 23:25:49 +0800

    add project week-6

--- modules/generate_key.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-def generate_complex_value():
-    result = subprocess.run(["openssl", "rand", "-base64", "32"], capture_output=True, text=True)
-    return result.stdout.strip()
-
-print(generate_complex_value())
